b72e8b76da64bc8cccb0f2febd6c1d8a644f4289
jeangalvis31 <jeangalvis31@gmail.com>
2025-05-31 11:48:40 -0500

    feat: :sparkles: Agregando paginacion a los generos y el homepage

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,19 +2,37 @@ import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 import { ProductsService } from '../../../products/services/products.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
+import { PaginationService } from '../../../shared/components/pagination/pagination.service';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent { 
   productServide = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+
+/*   activateRoute = inject(ActivatedRoute);
+
+  currenPage = toSignal(
+    this.activateRoute.queryParamMap.pipe(
+      map(params => (params.get('page') ? +params.get('page')! : 1)),
+      map(page => (isNaN(page) ? 1:page))
+    ),
+    {
+      initialValue: 1
+    }
+  ) */
 
   productsResource = rxResource({
-    request: () => ({}),
+    request: () => ({ page: this.paginationService.currenPage() -1}),
     loader: ({request}) => {
       return this.productServide.getProducts({
+        offset: request.page * 9
       });
     }
   })
